b9e248a856522d14108f375ea7cad3c5c8d54b82
Jose <jerickson576@gmail.com>
2023-12-01 18:27:47 -0500

    ultima prueba

--- src/main/java/com/proyecto/gym/Controller/AdministradorController.java
+++ src/main/java/com/proyecto/gym/Controller/AdministradorController.java
@@ -1,59 +1,20 @@
 package com.proyecto.gym.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.proyecto.gym.models.entity.Empleado;
-import com.proyecto.gym.models.service.IClienteService;
-import com.proyecto.gym.models.service.IEmpleadoService;
-import com.proyecto.gym.models.service.ISuscripcion;
-import com.proyecto.gym.models.service.IVisitaService;
-
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 
-    @Autowired
-    private IEmpleadoService empleadoService;
-
-    @Autowired
-    private IClienteService clienteService;
-
-    @Autowired
-    private ISuscripcion suscripcionService;
-
-    @Autowired
-    private IVisitaService visitaService;
+    
     
     @GetMapping({"/","","/index","/inicio"})
     public String index(Authentication auth, Model model){
-        //obtener el id del usuario logueado
-        Empleado empleado = empleadoService.buscarPorUsuario(auth.getName());
-        Double diaria = suscripcionService.ventaDiaria();
-        Double mensual = suscripcionService.ventaMensual();
-        String ventaDiaria, ventaMensual;
-        if(diaria == null){
-            ventaDiaria = "0.00";
-        }else{
-            ventaDiaria = String.format("%.2f", suscripcionService.ventaDiaria());
-        }
-
-        if(mensual == null){
-            ventaMensual = "0.00";
-        }else{
-            ventaMensual = String.format("%.2f", suscripcionService.ventaMensual());
-        }
-
-        model.addAttribute("empleado", empleado);
-        model.addAttribute("clientes", clienteService.listarClientesRecientes());
-        model.addAttribute("ventaDiaria", ventaDiaria);
-        model.addAttribute("ventaMensual", ventaMensual);
-        model.addAttribute("visitas", visitaService.visitasTotales());
-        model.addAttribute("totalSus", suscripcionService.suscripcionesTotales());
+        
         return "/view/administrador/index";
     }
 
